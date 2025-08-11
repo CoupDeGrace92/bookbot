0e56c260175f770a03ac395e072bab3a32bef8bc
CoupDeGrace92 <nickoboy1992@gmail.com>
2025-08-11 15:44:15 -0600

    Working version - outputs the letter count for Frankenstein in a dictionary

--- main.py
+++ main.py
@@ -5,22 +5,6 @@ def get_book_text(bookfile):
     return book
 
 
-def char_count(book):
-    word_list = book.split()
-    word_string = "".join(word_list)
-    word_list2 = word_string.lower()
-    character_list = {}
-    from stats import number_of_words
-    i = number_of_words(book)-1 #This will be our list of total elements in our word_list
-    for j in range(0,i):
-        word = word_list2[j]
-        length = len(word)
-        for k in range(0, length-1):
-            if word[k] in character_list:
-                character_list[word[k]] = character_list[word[k]] + 1
-            else:
-                character_list[word[k]] = 1
-    return character_list
 
 #The main function that will print the book to the console
 def main ():
@@ -28,7 +12,7 @@ def main ():
     from stats import number_of_words
     words = number_of_words(book)
     print (f"{words} words found in the document")
-    #from stats import char_count
+    from stats import char_count
     char_dict = char_count(book)
     print (char_dict)
     return
--- stats.py
+++ stats.py
@@ -9,16 +9,14 @@ def number_of_words(book):
 ##Function to count the number of instances of each letter##
 def char_count(book):
     word_list = book.split()
-    word_list2 = word_list.lower()
+    word_string = "".join(word_list)
+    word_string = word_string.lower()
     character_list = {}
-    i = number_of_words(book)-1 #This will be our list of total elements in our word_list
+    i = len(word_string)
     for j in range(0,i):
-        word = word_list2[j]
-        length = len(word)
-        for k in range(0, length-1):
-            if word[k] in character_list:
-                character_list[word[k]] = character_list[word[k]] + 1
+            if word_string[j] in character_list:
+                character_list[word_string[j]] += 1
             else:
-                character_list[word[k]] = 1
+                character_list[word_string[j]] = 1
     return character_list
                 
